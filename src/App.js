6b2327261e06b73e6293741cff75aab476b0b449
Hero Yudo Martono <hero@pens.ac.id>
2025-08-13 13:26:08 +0700

    Update fitur 4

--- src/App.js
+++ src/App.js
@@ -2,13 +2,43 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import AppHeader from "./components/header";
 import Button from 'react-bootstrap/Button';
 
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [message, setMessage] = useState('');
+
+  const fetchUser = async () => {
+    try {
+      const response = await axios.get('http://localhost:5000/user');
+      setMessage(response.data); // asumsi response.data = "hallo user"
+    } catch (error) {
+      setMessage('Error fetching user');
+      console.error(error);
+    }
+  };
+
+  const fetchLogin = async () => {
+    try {
+      const response = await axios.get('http://localhost:5001/login');
+      setMessage(response.data); // asumsi response.data = "hallo user"
+    } catch (error) {
+      setMessage('Error fetching login');
+      console.error(error);
+    }
+  };
+
+
   return (
     <div className="App">
       <header>
         <AppHeader />
       </header>
+      <Button variant="primary" onClick={fetchUser}>Primary</Button>
+      <Button variant="secondary" onClick={fetchLogin}>Secondary</Button>
+      <p>{message}</p>
+
+
     </div>
   );
 }
--- src/components/header.js
+++ src/components/header.js
@@ -7,12 +7,12 @@ function AppHeader() {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
-                <Navbar.Brand href="#home">Test</Navbar.Brand>
+                <Navbar.Brand href="#home"></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
+                        <Nav.Link href="#home">User</Nav.Link>
+                        <Nav.Link href="#link">Login</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
